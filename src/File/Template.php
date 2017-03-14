109c018dd7da8a445c066f3dba3835c471a0ab5c
Rik Meijer <ha.meijer@avans.nl>
2017-03-14 13:32:19 +0100

    reformat

--- src/File/Template.php
+++ src/File/Template.php
@@ -2,21 +2,23 @@
 namespace pulledbits\View\File;
 
 
-class Template implements \pulledbits\View\Template {
+class Template implements \pulledbits\View\Template
+{
     private $templatePath;
     private $layoutsPath;
     private $cachePath;
 
     private $helpers;
 
-    public function __construct(string $templatePath, string $layoutsPath, string $cachePath) {
+    public function __construct(string $templatePath, string $layoutsPath, string $cachePath)
+    {
         $this->templatePath = $templatePath;
         $this->layoutsPath = $layoutsPath;
         $this->cachePath = $cachePath;
         $this->helpers = [];
     }
 
-    public function capture(array $parameters) : string
+    public function capture(array $parameters): string
     {
         ob_start();
         $this->render($parameters);
@@ -25,17 +27,19 @@ class Template implements \pulledbits\View\Template {
 
     private function sub(string $templateIdentifier): \pulledbits\View\Template
     {
-        $template = new self(dirname($this->templatePath) . DIRECTORY_SEPARATOR . $templateIdentifier . '.php', $this->layoutsPath, $this->cachePath);
+        $template = new self(dirname($this->templatePath) . DIRECTORY_SEPARATOR . $templateIdentifier . '.php',
+            $this->layoutsPath, $this->cachePath);
         $template->helpers = $this->helpers;
         return $template;
     }
 
-    private function layout(string $layoutIdentifier) : \pulledbits\View\Layout
+    private function layout(string $layoutIdentifier): \pulledbits\View\Layout
     {
         return new Layout($this->layoutsPath . DIRECTORY_SEPARATOR . $layoutIdentifier . '.php');
     }
 
-    private function escape(string $unsafestring) {
+    private function escape(string $unsafestring)
+    {
         return htmlentities($unsafestring);
     }
 
@@ -63,18 +67,20 @@ class Template implements \pulledbits\View\Template {
         }
     }
 
-    private function captureHelper(callable $helper, array $arguments) : string {
+    private function captureHelper(callable $helper, array $arguments): string
+    {
         ob_start();
         call_user_func_array($helper, $arguments);
         return ob_get_clean();
     }
 
-    public function registerHelper(string $identifier, callable $callback) : void
+    public function registerHelper(string $identifier, callable $callback): void
     {
         $this->helpers[$identifier] = \Closure::bind($callback, $this, __CLASS__);
     }
 
-    public function render(array $parameters) : void {
+    public function render(array $parameters): void
+    {
         $variables = [];
         foreach ($parameters as $parameterIdentifier => $parameter) {
             if (is_callable($parameter)) {
@@ -88,7 +94,8 @@ class Template implements \pulledbits\View\Template {
         $cacheFile = $this->cachePath . DIRECTORY_SEPARATOR . sha1_file($this->templatePath) . '.php';
         if (file_exists($cacheFile) === false) {
             $contents = file_get_contents($this->templatePath);
-            file_put_contents($cacheFile, preg_replace('/<\?=\s?(.*?)[;\s]*\?>/', '<?=$this->escape($1);?>', $contents));
+            file_put_contents($cacheFile,
+                preg_replace('/<\?=\s?(.*?)[;\s]*\?>/', '<?=$this->escape($1);?>', $contents));
         }
         include $cacheFile;
     }
